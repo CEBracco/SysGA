f64c04639e8415a34225104b4fbefdf0bfef5a7c
CEBracco <bracco66.cb@gmail.com>
2017-07-19 00:11:02 -0300

    Manejo de usuarios: Configuracio inicial con fosUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,134 @@
+<?php
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+/**
+ * User
+ * @ORM\Table(name="user")
+ * @ORM\AttributeOverrides({
+ *      @ORM\AttributeOverride(name="email", column=@ORM\Column(type="string", name="email", length=255, unique=false, nullable=true)),
+ *      @ORM\AttributeOverride(name="emailCanonical", column=@ORM\Column(type="string", name="email_canonical", length=255, unique=false, nullable=true))
+ * })
+ * @ORM\Entity
+ */
+class User extends BaseUser{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+   /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=50, nullable=true)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellido", type="string", length=50, nullable=true)
+     */
+    private $apellido;
+
+	/**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return User
+     */
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre(){
+		return $this->nombre;
+    }
+
+
+    /**
+     * Set apellido
+     *
+     * @param string $apellido
+     *
+     * @return User
+     */
+    public function setApellido($apellido){
+        $this->apellido = $apellido;
+        return $this;
+    }
+
+    /**
+     * Get apellido
+     *
+     * @return string
+     */
+    public function getApellido(){
+		return $this->apellido;
+    }
+	/*
+    public function __construct(){
+        parent::__construct();
+    }
+	*/
+
+	public function setRol($rol){
+		$this->addRole($rol);
+	}
+
+	public function getRol(){
+		return $this->getRoles()[0];
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+	public function serializar(){
+		return array(
+			'apellido' => $this->getApellido(),
+			'nombre' => $this->getNombre(),
+			'rol' => $this->getRol(),
+			'username' => $this->getUsername());
+	}
+
+	function getRolAImprimir(){
+		$rol = $this->getRol();
+		switch($rol){
+			case "ROLE_SUPER_ADMIN": return "Super-Admin";
+			case "ROLE_ADMIN": return "Administrador";
+			case "ROLE_GESTION": return "Gestión";
+			case "ROLE_CONCESIONARIA": return "Concesionaria";
+			default: throw new \Exception("El rol '$rol' no tiene una entrada en el switch del método getRolAImprimir() de la clase User");
+		}
+	}
+
+	function getNivelDeJerarquia(){ //Se usa para ordenar a los usuarios en un listado segun su jerarquia
+		$rol = $this->getRol();
+		switch($rol){
+			case "ROLE_SUPER_ADMIN": return 4;
+			case "ROLE_ADMIN": return 3;
+			case "ROLE_GESTION": return 2;
+			case "ROLE_CONCESIONARIA": return 1;
+			default: throw new \Exception("El rol '$rol' no tiene una entrada en el switch del método getNivelDeJerarquia() de la clase User");
+		}
+	}
+}
