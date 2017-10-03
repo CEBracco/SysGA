09c261709ddd6888a759b9905c896e8f18715141
CEBracco <bracco66.cb@gmail.com>
2017-10-02 21:28:17 -0300

    Edicion de Tramite: no redireccionar al listado una vez guardado

--- src/AppBundle/Controller/TramiteController.php
+++ src/AppBundle/Controller/TramiteController.php
@@ -180,13 +180,19 @@ class TramiteController extends Controller
 			$em=$this->getDoctrine()->getManager();
 			$em->flush();
 
-            return $this->redirectToRoute('tramite_index');
+			return $this->redirectToRoute('tramite_edit', array('id' => $tramite->getId(),'status' => 'ok'));
         }
+
+		$msg=null;
+		if($request->query->get('status') == 'ok'){
+			$msg="Los cambios han sido guardados";
+		}
 
         return $this->render('tramite/form.html.twig', array(
             'tramite' => $tramite,
             'form' => $editForm->createView(),
             'edit' => true,
+			'msg' => $msg,
         ));
     }
 
